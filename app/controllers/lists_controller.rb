8eebaef8c811441709fc4e00b80f10fe6020645b
EC2 Default User <ec2-user@ip-172-31-20-69.ap-northeast-1.compute.internal>
2024-05-06 13:21:41 +0000

    2章　投稿機能

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,18 @@
 class ListsController < ApplicationController
   def new
+     # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
+    @list = List.new  
   end
+
+ def create #投稿を保存するためのcreateアクション
+   # １.&2. データを受け取り新規登録するためのインスタンス作成
+    list = List.new(list_params) #viewに渡さないから＠Listじゃない。ローカル変数
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    # 4. トップ画面へリダイレクト　redirect_to」は画面を遷移するためのメソッド
+    redirect_to '/top'
+ end 
+
 
   def index
   end
@@ -10,4 +22,14 @@ class ListsController < ApplicationController
 
   def edit
   end
+  
+  private # ストロングパラメータ
+  #privateは一種の境界線で、「ここから下はこのcontrollerの中でしか呼び出せません」てゆう意味
+  #privateより後に定義されたメソッドは、アクションとして認識されなくなり、URLと対応できなくなる
+  def 
+    list_params #formから送られてくるデータはparamsの中に入っています。
+    params.require(:list).permit(:title, :body)
+    # require. 送られてきたデータの中からモデル名(ここでは:list)を指定し、データを絞り込みます。
+    # permit.   requireで絞り込んだデータの中から、保存を許可するカラムを指定します。
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'lists/new'
+  get 'lists/new'  #GETはデータの取得をするためのHTTPメソッド
+  post 'lists' => 'lists#create' #POSTはデータを新規保存するためのHTTPメソッド
   get 'lists/index'
   get 'lists/show'
   get 'lists/edit'
-  get 'homes/top'
+  get 'top' => 'homes#top'
 #  get '/top', to:  'homes#top'.  #homes/topで打ってなかったからダメだった。URL通り打つこと
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
